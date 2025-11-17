58e290d8c85c87380ab0ce9652a8a98fec8dcec9
Shubham Kumar <shubhampal7083@gmail.com>
2025-11-17 22:51:55 +0530

    project name changed

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -6,13 +6,13 @@ export default function About() {
         <section id="About" className="py-20 bg-charcoal text-center px-6">
             <SectionWrapper>
                 <SectionInfo
-                    title={'About VyomGarud'}
+                    title={'About Garud One'}
                     description={'Engineering the future of autonomous flight'}
                 />
 
                 <div className="max-w-2xl mx-auto text-center">
                     <p className="text-lg md:text-xl leading-relaxed text-gray-300">
-                        VyomGarud pioneers next-generation UAV technology, combining AI-driven autonomy with aerospace-grade reliability. Our mission is to empower defense and industry with intelligent, resilient, and mission-ready aerial systems that safeguard horizons worldwide.
+                        GarudOne is a next-generation UAV technology, combining AI-driven autonomy with aerospace-grade reliability. Our mission is to empower defense and industry with intelligent, resilient, and mission-ready aerial systems that safeguard horizons worldwide.
                     </p>
                 </div>
             </SectionWrapper>
