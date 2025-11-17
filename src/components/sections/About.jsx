b8b61e1b6a7f701d0035e755aaa47c65770cb3fe
shubham <shubhampal7083@gmail.com>
2025-11-17 23:53:48 +0530

    preview added and error fixed

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -12,7 +12,7 @@ export default function About() {
 
                 <div className="max-w-2xl mx-auto text-center">
                     <p className="text-lg md:text-xl leading-relaxed text-gray-300">
-                        GarudOne is a next-generation UAV technology, combining AI-driven autonomy with aerospace-grade reliability. Our mission is to empower defense and industry with intelligent, resilient, and mission-ready aerial systems that safeguard horizons worldwide.
+                        VyomGarud is a next-generation UAV technology, combining AI-driven autonomy with aerospace-grade reliability. Our mission is to empower defense and industry with intelligent, resilient, and mission-ready aerial systems that safeguard horizons worldwide.
                     </p>
                 </div>
             </SectionWrapper>
